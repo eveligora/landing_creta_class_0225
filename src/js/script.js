4b0685f6a6bef13a59a73b5471439ba1a1a7d653
Yevhenii <Yevhenii.Boliekhovskyi@promx.net>
2025-05-15 21:36:59 +0300

    feat: learning_slider init

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-const body = document.querySelector('body');
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 3,
+	loop: true,
+	spaceBetween: 40,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.learningSliderNext',
+		prevEl: '.learningSliderPrev',
+	},
+
+	pagination: {
+		el: '.learningSliderPrevPag',
+		clickable: true,
+	},
+});
