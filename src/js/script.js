4785fc97c9bfa8186f043c8186b68e6209898e34
Yevhenii <Yevhenii.Boliekhovskyi@promx.net>
2025-05-26 21:25:15 +0300

    feat: header + scroll

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,15 @@ const swiper = new Swiper('.swiper', {
 		clickable: true,
 	},
 });
+
+const header = document.querySelector('.header');
+const toggleClass = 'on_scroll';
+
+window.addEventListener('scroll', () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll > 50) {
+		header.classList.add(toggleClass);
+	} else {
+		header.classList.remove(toggleClass);
+	}
+});
